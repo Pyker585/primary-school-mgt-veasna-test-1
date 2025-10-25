2bd8c08952fba77671dd2b91df5b9d1e848a7f14
Veasna <chanthakhin585@gmail.com>
2025-10-25 09:59:17 +0700

    Second commit

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <HelloWorld msg="See you on the other side"></HelloWorld>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>Hi if you see it mean your are successful</h1>
   </div>
 </template>
 
